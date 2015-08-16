fe365d5575e42c04150dbc1627b7387a79296c00
thomas15v <tvanmellaerts@gmail.com>
2015-08-16 09:49:00 +0200

    Clean source from version 1.0.4

---
+++ src/main/java/com/whitefirex/microjump/MicroJump.java
@@ -0,0 +1,42 @@
+package com.whitefirex.microjump;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MicroJump extends JavaPlugin
+{
+    public void onEnable()
+    {
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if ((sender instanceof Player)) {
+            if (args.length == 0) {
+                return false;
+            }
+
+            Player p = (Player)sender;
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(b);
+            try {
+                out.writeUTF("Connect");
+                out.writeUTF(args[0]);
+            }
+            catch (IOException localIOException)
+            {
+            }
+            p.sendPluginMessage(this, "BungeeCord", b.toByteArray());
+            return true;
+        }
+
+        return false;
+    }
+}
